d28178b231f11e2281bd9fab4586f615bde60843
LimSihwan <tlghks1009@naver.com>
2017-04-05 17:52:08 +0900

    - 구글 연동

--- Assets/_Scripts/Google/GoogleLogin.cs
+++ Assets/_Scripts/Google/GoogleLogin.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class GoogleLogin : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Awake ()
+	{
+		GoogleManager.GetInstance.InitializeGPGS();
+
+		if(GoogleManager.GetInstance.CheckLogin())
+			GoogleManager.GetInstance.LoginGPGS();
 	}
 }
